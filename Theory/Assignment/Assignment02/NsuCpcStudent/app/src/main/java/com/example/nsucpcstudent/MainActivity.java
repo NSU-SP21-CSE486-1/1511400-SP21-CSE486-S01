63fe99c997b4cf9918c3ca0d5a96e292cecabba4
Masud Rana <rana09.masud@gmail.com>
2021-03-27 22:10:19 +0600

    Assignment 02- Code Milestone 01: Student create landing screen #30
    landing screen with navigation

--- Theory/Assignment/Assignment02/NsuCpcStudent/app/src/main/java/com/example/nsucpcstudent/MainActivity.java
+++ Theory/Assignment/Assignment02/NsuCpcStudent/app/src/main/java/com/example/nsucpcstudent/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.nsucpcstudent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchRegScreen(View view) {
+        Intent regIntent = new Intent(this, RegistrationFirstStep.class);
+        startActivity(regIntent);
+    }
+
+    public void launchListScreen(View view) {
+        Intent listIntent = new Intent(this, StudentList.class);
+        startActivity(listIntent);
+    }
+
+    public void launchSearchScreen(View view) {
+        Intent searchIntent = new Intent(this, SearchFilter.class);
+        startActivity(searchIntent);
     }
 }
