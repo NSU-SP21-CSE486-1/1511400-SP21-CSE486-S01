57e49dfe419a560ae19a94ff7805d035d6e50a2c
Masud Rana <rana09.masud@gmail.com>
2021-04-04 02:38:53 +0600

    Assignment 02- Code Milestone 01: Student Registration Third step screen #23

--- Theory/Assignment/Assignment02/NsuCpcStudent/app/src/main/java/com/example/nsucpcstudent/RegistrationSecondStep.java
+++ Theory/Assignment/Assignment02/NsuCpcStudent/app/src/main/java/com/example/nsucpcstudent/RegistrationSecondStep.java
@@ -1,17 +1,39 @@
 package com.example.nsucpcstudent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+//import java.security.acl.Group;
 
 public class RegistrationSecondStep extends AppCompatActivity {
-
+    private TextView title;
+    private SwitchMaterial addressSwitch;
+    private Group addressGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_second_step);
+        addressSwitch= findViewById(R.id.same_address);
+        addressGroup=findViewById(R.id.group);
+        title=findViewById(R.id.permanent_address_title);
+        addressSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    addressGroup.setVisibility(View.GONE);
+                }else {
+                    addressGroup.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     public void launchThirdStepScreen(View view) {
@@ -23,4 +45,6 @@ public class RegistrationSecondStep extends AppCompatActivity {
         Intent firstIntentScreen = new Intent(this, RegistrationFirstStep.class);
         startActivity(firstIntentScreen);
     }
+
+
 }
--- Theory/Assignment/Assignment02/NsuCpcStudent/app/src/main/java/com/example/nsucpcstudent/SuccessScreen.java
+++ Theory/Assignment/Assignment02/NsuCpcStudent/app/src/main/java/com/example/nsucpcstudent/SuccessScreen.java
@@ -2,7 +2,9 @@ package com.example.nsucpcstudent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SuccessScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SuccessScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_screen);
+    }
+
+    public void goToHomeScreen(View view) {
+        Intent homeIntent = new Intent(this, MainActivity.class);
+        startActivity(homeIntent);
     }
 }
